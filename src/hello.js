837b303eba8471684564fe8ee71397f3ea7edff0
胡迪超 <hu.dichao@gmail.com>
2016-04-15 16:09:08 +0800

    用新版教材重来

--- src/hello.js
+++
@@ -1,7 +0,0 @@
-// function sayHello() {
-//   return "hello fucking world";
-// }
-
-function sayHello(to) {
-  return _.template("Hello, <%= name %>!")({name: to});
-}
--- test/hello_spce.js
+++
@@ -1,8 +0,0 @@
-describe("Hello", function() {
-  // it("says hello", function() {
-  //   expect(sayHello()).toBe("hello fucking world");
-  // });
-  it("says hello to receiver", function() {
-    expect(sayHello("Bill")).toBe("Hello, Bill");
-  }); 
-});
