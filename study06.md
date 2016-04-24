2499378157c7b6c7c778ec582c2ccf5a597f4868
胡迪超 <hu.dichao@gmail.com>
2016-04-25 00:57:22 +0800

    科学计数法

--- study06.md
+++ study06.md
@@ -23,3 +23,7 @@ W054: JSHint warning code 『the function constructor is a form of eval』
 
 在lex中加一个peek函数，读取下一个char的值
 
+e相关的，本来js就支持，直接让js搞就行了。
+
+但是这样会导致parse一些不该parse的 ---> 导入exp operator
+
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -18,6 +18,30 @@ describe("parse", function() {
   it("parse没有整数的小数", function() {
     var fn = parse(".42");
     expect(fn()).toBe(0.42);
+  });
 
+  it("parse科学计数法数字", function() {
+    var fn = parse("42e3");
+    expect(fn()).toBe(42000);
+  });
+  it("parse科学计数法数字2", function() {
+    var fn = parse(".42e2");
+    expect(fn()).toBe(42);
+  });
+  it("parse科学计数法3", function() {
+    var fn = parse("4200e-2");
+    expect(fn()).toBe(42);
+  });
+  it("parse科学计数法4", function() {
+    var fn = parse(".42e+2");
+    expect(fn()).toBe(42);
+  });
+  it("parse科学计数法5", function() {
+    var fn = parse(".42E2");
+    expect(fn()).toBe(42);
+  });
+  it("parse科学计数法6,不能parse下面这种", function() {
+    expect(function() {parse("42e-");}).toThrow();
+    expect(function() {parse("42e-a");}).toThrow();
   });
 });
